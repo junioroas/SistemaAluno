fda4a1014c1ac19d5aedab2f7dc699e4400a159b
Osmar Junior <junior.oas.oa@gmail.com>
2022-04-17 21:29:31 -0300

    SistemaAluno

---
+++ alunos.js
@@ -0,0 +1,50 @@
+//Usando For of
+
+let nomeDeAluno = ["Marcel", "Franklin", "Rita", "Afrânio"]
+
+for (let nome of nomeDeAluno){
+    console.log(`Esta pessoa se chama ${nome}`)
+}
+    
+
+//Usando While:
+
+// let numeroDeAlunos = 10; //Declaração do nº de alunos
+
+// let contador = 0 ; //Declaração do contador
+
+//  while(contador <= numeroDeAlunos) {
+//      //console.log (contador)
+
+//      if(contador == 0){
+//          console.log("O número atual é zero!")
+//      }else if(contador % 2 != 0){
+//          console.log("O número " + contador + " é Ímpar!")
+//      }else{
+//          console.log(`O número ${contador} é Par!`)
+//      }
+//      contador++; //Contador no While após a sequência If/Else
+//  }
+
+
+
+
+// Usando If/Else:
+
+// let numeroDeAlunos = 10;
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//     //console.log (contador)
+
+//     if(contador == 0){
+//         console.log("O número atual é zero!")
+//     }else if(contador % 2 == 0){
+//         console.log("O número " + contador + " é Par!")
+//     }else{
+//         console.log(`O número ${contador} é Ímpar!`)
+//     }
+// }
+
+//Seleciona todo o código -> Ctrl + ; para comentar todo o código
+
+
